67c59cdbb8858ba4d9e5104d13f5d5af298a0a95
RodrigoCanteraYepes <rocantera04@gmail.com>
2025-07-16 11:07:03 +0200

    Remove domain attribute from product_id fields to fix ParseError

--- models/sale_order_chapter.py
+++ models/sale_order_chapter.py
@@ -217,7 +217,6 @@ class SaleOrderChapterLine(models.Model):
     product_id = fields.Many2one(
         'product.product',
         string='Producto',
-        domain="_get_product_domain",
         ondelete='set null'
     )
     
@@ -968,7 +967,6 @@ class SaleOrderChapterTemplateLine(models.Model):
     product_id = fields.Many2one(
         'product.product',
         string='Producto',
-        domain="_get_product_domain",
         ondelete='set null'
     )
     
